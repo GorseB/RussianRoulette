ca7a89aa23823f53a34235c2523b13e5930b9ad3
GorseB <josiahkerr@hotmail.com>
2016-03-22 12:49:25 +1300

    Added files via upload
    V1.1

---
+++ RussianRoulette-master/RussianRoulette/RussianRoulette/GameStats.cs
@@ -0,0 +1,15 @@
+﻿namespace RussianRoulette
+{
+    internal static class GameStats
+    {
+        public static int GamesPlayed = 0;
+        public static int Won = 0;
+        public static int Lost = 0;
+        public static int Streak = 0;
+        public static bool LostLast = false;
+        public static int RouletteMissles = 0;
+        public static bool Isplaying = false;
+        // Most of our globals are kept here, so that everyone can see them \(o_o)/
+        public static string[] textinfo = new string[3];
+    }
+}
