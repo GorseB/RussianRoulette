c0f28b304aca3bd0fb2b8ef5b6fa7a5bb6917f26
Josiah Kerr <josiahkerr@hotmail.com>
2019-01-29 15:57:49 +1300

    Fixed Addressing bug

---
+++ UnitTestProject1/Test Loading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestBulletLoading()
+        {
+            var RMISSLES = new int();
+            RMISSLES = 0;
+            
+            if (RMISSLES == 0)
+            {
+                RMISSLES = 6;
+                Assert.AreEqual(6, RMISSLES);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
